64fa02e0b293c50c65558d4137b0a86cea73327f
Adam <adam@ofadam.com>
2025-02-19 22:51:49 -0600

    Further cleanup

--- rrdtograph.py
+++ rrdtograph.py
@@ -81,7 +81,7 @@ monthstep = 7 * weekstep  #Monthly graph stepsize
 
 #Prepare graph
 #Current date/time/midnight/sunset/sunrise
-ct = datetime.now(timezone.utc) # corrected line.
+ct = datetime.now(timezone.utc)
 ct_str = ct.strftime("%Y-%m-%d %H:%M") #created new variable for string format.
 print("Preparing graphs: ",ct_str)
 #Calc epoch of last midnight - need correction for local time ( - 1 hour)
